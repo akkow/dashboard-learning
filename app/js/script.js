b7da00d9415b8c6558b668057a6cc8675d6419ba
Benas Švarckis <birdxxteam@gmail.com>
2023-05-10 14:16:07 +0300

    pabaiga

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,56 @@
+const DarkButton = document.getElementById('dark');
+const LightButton = document.getElementById('light');
+
+const setColorMode = () => {
+    if(localStorage.getItem('colorMode') == 'dark') {
+        setDarkMode();
+        DarkButton.click();
+    } else if(localStorage.getItem('colorMode') == 'light'){
+        setLightMode();
+        LightButton.click();
+    }
+};
+
+const checkMode = () => {
+    if (localStorage.getItem('colorMod') == null) {
+        if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+            LightButton.click();
+        }
+        else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            DarkButton.click();
+        }
+    }
+};
+
+const checkModeChange = () => {
+    window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+        checkMode();
+    });
+};
+
+const setDarkMode = () => {
+    document.querySelector('body').classList = 'dark'
+};
+
+const setLightMode = () => {
+    document.querySelector('body').classList = 'light'
+};
+
+setColorMode();
+checkMode();
+checkModeChange();
+
 const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+
 for(let i = 0; i < radioButtons.length; i++){
     radioButtons[i].addEventListener('click', event => {
-        document.getElementById('dark').checked
-     ? (document.querySelector('body').classList = 'dark')
-     : (document.querySelector('body').classList = 'light');
-    })
-};
+        if(DarkButton.checked) {
+            localStorage.setItem('colorMode', 'dark');
+            setDarkMode();
+        } else {
+            localStorage.setItem('colorMode', 'light');
+            setLightMode();
+        }
+    });
+}
